a3c4202aa3403209c073bad58432e39305396a52
Eric Bauer <eric.bauer55@gmail.com>
2022-02-12 16:05:32 -0500

    added a ride_id function and other csv read utility

--- src/utils/extract.py
+++ src/utils/extract.py
@@ -3,8 +3,7 @@ import gpxpy as gx
 
 def read_gpx_to_dataframe(file_path):
     # Extract the ride ID from the file_path
-    ride_id = file_path.split('/')[-1] # grab the file name from the path
-    ride_id = ride_id.split('.gpx')[0] # grab the ride ID from file name
+    ride_id = get_ride_id(file_path=file_path)
 
     # Setup data capture as lists initially
     data = []
@@ -25,5 +24,21 @@ def read_gpx_to_dataframe(file_path):
     
     # Capture the data structure as a Pandas Dataframe
     df = pd.DataFrame(data)
+
+    return df
+
+def get_ride_id(file_path):
+    # Extract the ride ID from the file_path
+    ride_id = file_path.split('/')[-1] # grab the file name from the path
+    ride_id = ride_id.split('.gpx')[0] # grab the ride ID from file name
+
+    return ride_id
+
+def read_ride_csv(file_path):
+    # Read in the CSV file for the Ride
+    df = pd.read_csv(file_path)
+    
+    # guarantee the timestamps are datetime objects
+    df['time'] = pd.to_datetime(df['time'])
 
     return df
