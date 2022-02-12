c974b96fcdc7d7ef0db2ce806a70a00803b41b0b
Eric Bauer <eric.bauer55@gmail.com>
2022-02-12 17:29:26 -0500

    removed the emoji from the raw file and re-ran ETL successfully

--- src/utils/extract.py
+++ src/utils/extract.py
@@ -11,19 +11,16 @@ def read_gpx_to_dataframe(file_path):
     # Open up the .gpx file and gather each point of data
     with open(file_path,'r') as opened_file:
         # parse the .gpx file into a GPX object
-        try:
-            gpx = gx.parse(opened_file)
+        gpx = gx.parse(opened_file)
         
-            # iterate through all tracks, segments and points to extract the data
-            for i, track in enumerate(gpx.tracks):
-                for j, segment in enumerate(track.segments):
-                    for point in segment.points:
-                        # create the row of data & append to data
-                        row = {'track':ride_id, 'segment_id':j, 'time':point.time, 
-                            'elevation':point.elevation, 'latitude':point.latitude, 'longitude':point.longitude}
-                        data.append(row)
-        except UnicodeDecodeError:
-            print(f'UnicodeDecodeError detected on file="{file_path}"')
+        # iterate through all tracks, segments and points to extract the data
+        for i, track in enumerate(gpx.tracks):
+            for j, segment in enumerate(track.segments):
+                for point in segment.points:
+                    # create the row of data & append to data
+                    row = {'track':ride_id, 'segment_id':j, 'time':point.time, 
+                        'elevation':point.elevation, 'latitude':point.latitude, 'longitude':point.longitude}
+                    data.append(row)
     
     # Capture the data structure as a Pandas Dataframe
     df = pd.DataFrame(data)
