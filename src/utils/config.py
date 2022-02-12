b6a40578880225186469ab12a699fc031d48aa6b
Eric Bauer <eric.bauer55@gmail.com>
2022-02-12 17:12:49 -0500

    edited the config to include the absolute root path

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,21 +1,25 @@
 import pandas as pd
+from os.path import join
 
 class Config():
     def __init__(self):
-        pass
+        # Store the absolute path of the project
+        # This should be edited if working in a new environment after repo cloning
+        self.root_dir = 'C:/Users/Demo/Documents/Data_Science/Strava/strava-fitness/'
+
 
     @property
     def raw_ride_path(self):
-        return '../data/raw/activities/'
+        return join(self.root_dir, 'data/raw/activities/')
 
     @property
     def extracted_ride_path(self):
-        return '../data/processed/activities/'
+        return join(self.root_dir, 'data/processed/activities/')
 
     @property
     def enriched_ride_path(self):
-        return '../data/enriched/activities/'
+        return join(self.root_dir, 'data/enriched/activities/')
 
     @property
     def activity_log_path(self):
-        return '../data/processed/activity_log.csv'
+        return join(self.root_dir, 'data/processed/activity_log.csv')
