015c6ef3709befc665eb542e7e5f10095e66d6d6
Eric Bauer <eric.bauer55@gmail.com>
2022-02-13 04:27:25 -0500

    working on the violation removal

--- src/utils/transform/clean.py
+++ src/utils/transform/clean.py
@@ -6,12 +6,13 @@ class PrivacyZoner():
         self.df = df
         self.privacy_zone_path = privacy_zone_path
         self.df_privacy = None
+        self.temporary_prox_columns = []
 
     def run(self):
         self._read_privacy_zones()
         self._calculate_proximities()
-        self._flag_zone_breaches()
-        self._remove_breach_gps_data()
+        self._remove_violation_gps_data()
+        self._drop_temporary_prox_columns()
 
     ################################################################
     # PROCESS METHODS
@@ -22,12 +23,25 @@ class PrivacyZoner():
 
     def _calculate_proximities(self):
         for address_k in self.df_privacy.shape[0]:
+            # get the relevant parameters
             dist_name = 'prox_' + self.df_privacy.loc[address_k, 'name']
             latitude = self.df_privacy.loc[address_k, 'latitude']
             longitude = self.df_privacy.loc[address_k, 'longitude']
-
+            
+            # calculate the proximity
             self.df[dist_name] = self._get_proximity_to_address(latitude, longitude)
+            self.temporary_prox_columns.append(dist_name)
 
+    def _remove_violation_gps_data(self):
+        for address_k in self.df_privacy.shape[0]:
+             # get the relevant parameters
+            dist_name = 'prox_' + self.df_privacy.loc[address_k, 'name']
+            privacy_radius = self.df_privacy.loc[address_k, 'privacy_radius']
+
+            
+    
+    def _drop_temporary_prox_columns(self):
+        self.df.drop(self.temporary_prox_columns, axis=1, inplace=True)
 
     ################################################################
     # HELPER METHODS
