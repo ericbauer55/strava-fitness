c20537ef012fda732c4d486a8c01758197ba9f74
Eric Bauer <eric.bauer55@gmail.com>
2022-02-13 04:06:29 -0500

    starting the work on the privacy zoning

---
+++ src/utils/transform/clean.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from haversine import haversine
+
+class PrivacyZoner():
+    def __init__(self, df, priacy_zone_path):
+        self.df = df
+        
+
+    def run(self):
+        pass
+
+    ################################################################
+    # PROCESS METHODS
+    ################################################################
+
+
+
+    ################################################################
+    # HELPER METHODS
+    ################################################################
+
+    def _read_privacy_zones(self):
+        pass
