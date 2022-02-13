d97a394638639ea049abaa7903ae493009e1f2ff
Eric Bauer <eric.bauer55@gmail.com>
2022-02-12 23:07:00 -0500

    successfully refactored the current ETL pipeline to run with cleaner process methods

--- src/utils/etl.py
+++ src/utils/etl.py
@@ -29,47 +29,18 @@ class RideETL():
         """
         This method converts all valid raw GPX files found in the Config's Raw_Ride_Path into .CSV files
         """
-        # Get the list of raw activity files
-        raw_rides_path = self.config.raw_ride_path
-        raw_ride_files = listdir(raw_rides_path) # get all files and directories
-        raw_ride_files = [join(raw_rides_path, f) for f in raw_ride_files if f != '.gitignore'] # add full paths to files
-        raw_ride_files = [f for f in raw_ride_files if isfile(f)] # get only files, no directories
+        # Define the details of the process
+        process_details_dict = {'process_func': None,
+                                'extract_func': read_gpx_to_dataframe,
+                                'input_path': self.config.raw_ride_path,
+                                'output_path': self.config.extracted_ride_path,
+                                'filter_valid': True,
+                                'description_template': 'Extracting {} GPX ride files to CSV'
+                                } 
 
-        # Filter the activity files for only the valid ones
-        valid_raw_ride_files = self._select_valid_rides(raw_ride_files)
+        self.apply_process(process_details_dict=process_details_dict)
 
-        print(f'Extracting {len(valid_raw_ride_files)} GPX ride files to CSV')
-        for ride_file in tqdm(valid_raw_ride_files):
-            # Read the Ride's GPX file
-            df = read_gpx_to_dataframe(ride_file)
-
-            # Build the new file name for PROCESSED data
-            ride_id = get_ride_id(ride_file)
-            new_file_name = join(self.config.extracted_ride_path, (str(ride_id)+'.csv'))
-
-            # Write the Ride's CSV file
-            df.to_csv(new_file_name, index=False)
-
-    def _select_valid_rides(self, file_names):
-        """
-        Given a list of @file_names of potential ride files, this method refers to the processed Activity Log.
-        For all files with ride_id's in the Log, these ride files are retained as valid rides and returned.
-        """
-        # Read the list of non-manually uploaded, Ride-type activities
-        df_log_valid = pd.read_csv(self.config.activity_log_path)
-        df_log_valid = df_log_valid['ride_id'].to_frame() # select only the ride_id column
-
-        # Build the @file_names into a dataframe of "ride_id" | "file_name" columns
-        data = [{'ride_id':int(get_ride_id(ride_file)), 'file_name':ride_file} for ride_file in file_names]
-        df_files = pd.DataFrame(data=data)
-
-        # Subset the ride_ids that appear in df_log_valid
-        df_valid = df_log_valid.merge(df_files, on='ride_id', how='inner')
-
-        # Return the list of file names that correspond to valid ride_ids
-        valid_file_names = list(df_valid['file_name'].values)
-        return valid_file_names
-
+    
     ############################################################################################
     # TRANSFORM
     ############################################################################################
@@ -99,6 +70,26 @@ class RideETL():
     ############################################################################################
     # HELPERS
     ############################################################################################
+
+    def _select_valid_rides(self, file_names):
+        """
+        Given a list of @file_names of potential ride files, this method refers to the processed Activity Log.
+        For all files with ride_id's in the Log, these ride files are retained as valid rides and returned.
+        """
+        # Read the list of non-manually uploaded, Ride-type activities
+        df_log_valid = pd.read_csv(self.config.activity_log_path)
+        df_log_valid = df_log_valid['ride_id'].to_frame() # select only the ride_id column
+
+        # Build the @file_names into a dataframe of "ride_id" | "file_name" columns
+        data = [{'ride_id':int(get_ride_id(ride_file)), 'file_name':ride_file} for ride_file in file_names]
+        df_files = pd.DataFrame(data=data)
+
+        # Subset the ride_ids that appear in df_log_valid
+        df_valid = df_log_valid.merge(df_files, on='ride_id', how='inner')
+
+        # Return the list of file names that correspond to valid ride_ids
+        valid_file_names = list(df_valid['file_name'].values)
+        return valid_file_names
 
     def apply_process(self, process_details_dict):
         """
