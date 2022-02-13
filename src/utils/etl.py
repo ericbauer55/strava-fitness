dd6d0984c986e64ae146b481a7be37d5060d7f14
Eric Bauer <eric.bauer55@gmail.com>
2022-02-12 22:38:02 -0500

    finishing off the time normalization

--- src/utils/etl.py
+++ src/utils/etl.py
@@ -18,7 +18,7 @@ class RideETL():
         """
         This is the high-level interface method to run the ETL pipeline in its correct sequence
         """
-        #self.extract_gpx_to_csv()
+        self.extract_gpx_to_csv()
         self.normalize_time_sampling()
 
     ############################################################################################
